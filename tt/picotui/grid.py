8878cc1ac2cb7612b448ab036e59f2a5c8375ecc
igorkarpov <P@ssw0rd>
2018-08-01 14:59:14 +0300

    grid rawing and handling enhancements

--- tt/picotui/grid.py
+++ tt/picotui/grid.py
@@ -1,4 +1,5 @@
-from picotui.basewidget import Widget
+from picotui.basewidget import FocusableWidget, ACTION_CANCEL
+from picotui.defs import *
 from picotui.screen import Screen
 
 P_FIRST = 0
@@ -6,42 +7,44 @@ P_NONE  = 1
 P_STOP  = 2
 P_LAST  = 3
 
+KIND_SINGLE = 0
+KIND_DOUBLE = 1
+
 # for every sub-array: [vertical position * 4 + horizontal position]
 # contains duplicates, but easily addressable
 CHARS = [
-  # vertical: single, horizontal: single
-  [
-    b'\xe2\x94\x8c', b'\xe2\x94\x80', b'\xe2\x94\xac', b'\xe2\x94\x90',
-    b'\xe2\x94\x82', b' ',            b'\xe2\x94\x82', b'\xe2\x94\x82',
-    b'\xe2\x94\x9c', b'\xe2\x94\x80', b'\xe2\x94\xbc', b'\xe2\x94\xa4',
-    b'\xe2\x94\x94', b'\xe2\x94\x80', b'\xe2\x94\xb4', b'\xe2\x94\x98'
-  ],
-  # vertical: single, horizontal: double
-  [
-    b'\xe2\x95\x92', b'\xe2\x95\x90', b'\xe2\x95\xa4', b'\xe2\x95\x95',
-    b'\xe2\x94\x82', b' ',            b'\xe2\x94\x82', b'\xe2\x94\x82',
-    b'\xe2\x95\x9e', b'\xe2\x95\x90', b'\xe2\x95\xaa', b'\xe2\x95\xa1',
-    b'\xe2\x95\x98', b'\xe2\x95\x90', b'\xe2\x95\xa7', b'\xe2\x95\x9b'
-  ],
-  # vertical: double, horizontal: single
-  [
-    b'\xe2\x95\x93', b'\xe2\x94\x80', b'\xe2\x95\xa5', b'\xe2\x95\xa6',
-    b'\xe2\x95\x91', b' ',            b'\xe2\x95\x91', b'\xe2\x95\x91',
-    b'\xe2\x95\x9f', b'\xe2\x94\x80', b'\xe2\x95\xab', b'\xe2\x95\xa2',
-    b'\xe2\x95\x99', b'\xe2\x94\x80', b'\xe2\x95\xa8', b'\xe2\x95\x9c'
-  ],
-  # vertical: double, horizontal: double
-  [
-    b'\xe2\x95\x94', b'\xe2\x95\x90', b'\xe2\x95\xa6', b'\xe2\x95\x97',
-    b'\xe2\x95\x91', b' ',            b'\xe2\x95\x91', b'\xe2\x95\x91',
-    b'\xe2\x95\xa0', b'\xe2\x95\x90', b'\xe2\x95\xac', b'\xe2\x95\xa3',
-    b'\xe2\x95\x9a', b'\xe2\x95\x90', b'\xe2\x95\xa9', b'\xe2\x95\x9d'
-  ]
+    # vertical: single, horizontal: single
+    [
+        b'\xe2\x94\x8c', b'\xe2\x94\x80', b'\xe2\x94\xac', b'\xe2\x94\x90',
+        b'\xe2\x94\x82', b' ',            b'\xe2\x94\x82', b'\xe2\x94\x82',
+        b'\xe2\x94\x9c', b'\xe2\x94\x80', b'\xe2\x94\xbc', b'\xe2\x94\xa4',
+        b'\xe2\x94\x94', b'\xe2\x94\x80', b'\xe2\x94\xb4', b'\xe2\x94\x98'
+    ],
+    # vertical: single, horizontal: double
+    [
+        b'\xe2\x95\x92', b'\xe2\x95\x90', b'\xe2\x95\xa4', b'\xe2\x95\x95',
+        b'\xe2\x94\x82', b' ',            b'\xe2\x94\x82', b'\xe2\x94\x82',
+        b'\xe2\x95\x9e', b'\xe2\x95\x90', b'\xe2\x95\xaa', b'\xe2\x95\xa1',
+        b'\xe2\x95\x98', b'\xe2\x95\x90', b'\xe2\x95\xa7', b'\xe2\x95\x9b'
+    ],
+    # vertical: double, horizontal: single
+    [
+        b'\xe2\x95\x93', b'\xe2\x94\x80', b'\xe2\x95\xa5', b'\xe2\x95\xa6',
+        b'\xe2\x95\x91', b' ',            b'\xe2\x95\x91', b'\xe2\x95\x91',
+        b'\xe2\x95\x9f', b'\xe2\x94\x80', b'\xe2\x95\xab', b'\xe2\x95\xa2',
+        b'\xe2\x95\x99', b'\xe2\x94\x80', b'\xe2\x95\xa8', b'\xe2\x95\x9c'
+    ],
+    # vertical: double, horizontal: double
+    [
+        b'\xe2\x95\x94', b'\xe2\x95\x90', b'\xe2\x95\xa6', b'\xe2\x95\x97',
+        b'\xe2\x95\x91', b' ',            b'\xe2\x95\x91', b'\xe2\x95\x91',
+        b'\xe2\x95\xa0', b'\xe2\x95\x90', b'\xe2\x95\xac', b'\xe2\x95\xa3',
+        b'\xe2\x95\x9a', b'\xe2\x95\x90', b'\xe2\x95\xa9', b'\xe2\x95\x9d'
+    ]
 ]
 
 
-def draw_box(left, top, w, h, top_kind, bottom_kind, left_kind, right_kind, h_stops, v_stops):
-
+def draw_grid(left, top, w, h, top_kind, bottom_kind, left_kind, right_kind, h_stops, v_stops):
     def draw_line(v_pos, y, current_h_kind):
         Screen.goto(left, y)
         h_stop = 0
@@ -84,13 +87,33 @@ def draw_box(left, top, w, h, top_kind, bottom_kind, left_kind, right_kind, h_st
     draw_line(P_LAST, top + h - 1, bottom_kind)
 
 
-
-
-class WGrid(Widget):
-
-    def __init__(self, w, h):
+class WGrid(FocusableWidget):
+    def __init__(self, w, h, column_widths=None):
+        super().__init__()
+        self.x = 0
+        self.y = 0
         self.w = w
         self.h = h
 
     def redraw(self):
-        draw_box(self.x, self.y, self.w, self.h, 1, 0, 1, 0, [(10, 1), (15, 0)], [(3,1), (5,0)])
+        self.cursor(False)
+        self.attr_color(C_B_CYAN, C_BLUE)
+        draw_grid(
+            self.x, self.y, self.w, self.h,
+            KIND_DOUBLE, KIND_DOUBLE, KIND_DOUBLE, KIND_DOUBLE,
+            [(10, KIND_DOUBLE), (15, KIND_DOUBLE)], [(3, KIND_DOUBLE), (5, KIND_SINGLE)]
+        )
+
+    def handle_mouse(self, x, y):
+        pass
+
+    def handle_key(self, key):
+        if key == KEY_QUIT:
+            return key
+        elif key == KEY_ESC:
+            return ACTION_CANCEL
+        elif key == KEY_ENTER:
+            self.signal("enter")
+
+    def on_enter(self):
+        pass
